0d4da833cac219accc4735c9f821708880526efa
Ryan Boyer <ryanjboyer@gmail.com>
2023-03-01 01:05:42 -0700

    ConfigurationManager tweaks

--- Editor/Odin Inspector/ConfigurationManager.cs
+++ Editor/Odin Inspector/ConfigurationManager.cs
@@ -6,20 +6,12 @@ using System.Linq;
 
 namespace Foundation.Editors {
     internal sealed class ConfigurationManager: OdinMenuEditorWindow {
-        private static Type[] typesToDisplay = TypeCache.GetTypesWithAttribute<ConfigurationDataAttribute>()
-            .OrderBy(c => c.Name)
-            .ToArray();
-
-        private Type selectedType;
-
         [MenuItem("Tools/Configuration Manager")]
         private static void OpenEditor() => GetWindow<ConfigurationManager>();
 
         protected override OdinMenuTree BuildMenuTree() {
             OdinMenuTree tree = new OdinMenuTree();
-            foreach (Type type in typesToDisplay) {
-                tree.AddAllAssetsAtPath(type.Name, "Assets/", type, true, true);
-            }
+            tree.AddAllAssetsAtPath("Configuration", "Assets/_Scriptable Objects/Configuration", typeof(AnyConfiguration), true, true);
             return tree;
         }
     }
