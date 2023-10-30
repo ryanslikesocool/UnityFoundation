d82454d2f43ff6c08fd83b9658930be6d34f3afc
ryan <ryanjboyer@gmail.com>
2023-10-30 03:09:18 -0600

    Extension cleanup

--- Scripts/Extensions/Extensions+Color.cs
+++ Scripts/Extensions/Extensions+Color.cs
@@ -3,23 +3,23 @@ using Unity.Mathematics;
 using UnityEngine;
 
 namespace Foundation {
-    public static partial class Extensions {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Color Opacity(this Color color, float opacity) {
-            color.a = opacity;
-            return color;
-        }
+	public static partial class Extensions {
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Color Opacity(this Color color, float opacity) {
+			color.a = opacity;
+			return color;
+		}
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Color HDR(this Color color, float hdr) {
-            color.r *= hdr;
-            color.g *= hdr;
-            color.b *= hdr;
-            return color;
-        }
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Color HDR(this Color color, float hdr) {
+			color.r *= hdr;
+			color.g *= hdr;
+			color.b *= hdr;
+			return color;
+		}
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool Approximately(this Color color, Color other, float epsilon = EPSILON4)
-            => color.ToFloat4().Approximately(other.ToFloat4(), epsilon);
-    }
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool Approximately(this Color color, Color other, float epsilon = EPSILON4)
+			=> color.AsFloat4().Approximately(other.AsFloat4(), epsilon);
+	}
 }
--- Scripts/Extensions/Extensions+Float4.cs
+++ Scripts/Extensions/Extensions+Float4.cs
@@ -3,33 +3,33 @@ using Unity.Mathematics;
 using UnityEngine;
 
 namespace Foundation {
-    public static partial class Extensions {
-        public static float4 Wrap(this float4 value, float4 min, float4 max) => new float4(
-            value.x.Wrap(min.x, max.x),
-            value.y.Wrap(min.y, max.y),
-            value.z.Wrap(min.z, max.z),
-            value.w.Wrap(min.w, max.w)
-        );
+	public static partial class Extensions {
+		public static float4 Wrap(this float4 value, float4 min, float4 max) => new float4(
+			value.x.Wrap(min.x, max.x),
+			value.y.Wrap(min.y, max.y),
+			value.z.Wrap(min.z, max.z),
+			value.w.Wrap(min.w, max.w)
+		);
 
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float4 ToFloat4(this Color color)
-            => new float4(color.r, color.g, color.b, color.a);
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float4 AsFloat4(this Color color)
+			=> new float4(color.r, color.g, color.b, color.a);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float4 RoundToNearest(this float4 input, float4 nearest)
-            => math.round(input / nearest) * nearest;
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float4 RoundToNearest(this float4 input, float4 nearest)
+			=> math.round(input / nearest) * nearest;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float4 FloorToNearest(this float4 input, float4 nearest)
-            => math.floor(input / nearest) * nearest;
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float4 FloorToNearest(this float4 input, float4 nearest)
+			=> math.floor(input / nearest) * nearest;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float4 CeilToNearest(this float4 input, float4 nearest)
-            => math.ceil(input / nearest) * nearest;
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float4 CeilToNearest(this float4 input, float4 nearest)
+			=> math.ceil(input / nearest) * nearest;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool Approximately(this float4 a, float4 b, float epsilon = EPSILON4)
-            => math.distancesq(a, b) < epsilon * epsilon;
-    }
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool Approximately(this float4 a, float4 b, float epsilon = EPSILON4)
+			=> math.distancesq(a, b) < epsilon * epsilon;
+	}
 }
