80cfd89568c2884c8076bb206431e4a94ec0905d
Ryan Boyer <ryanjboyer@gmail.com>
2023-10-29 21:16:58 -0600

    Add more gameobject extensions

--- Scripts/Extensions/Extensions+GameObject.cs
+++ Scripts/Extensions/Extensions+GameObject.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 namespace Foundation {
 	public partial class Extensions {
-		public static void AddChild(this GameObject parent, GameObject child) {
-			child.transform.parent = parent.transform;
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void AddChild(this GameObject parent, GameObject child)
+			=> child.transform.parent = parent.transform;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void AddChildren(this GameObject parent, in IEnumerable<GameObject> children)
+			=> children.ForEach(child => AddChild(parent, child));
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void RemoveFromParent(this GameObject child)
+			=> child.transform.parent = null;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool HasParent(this GameObject child, in Func<GameObject, bool> predicate) {
+			GameObject parent = child.transform.parent.gameObject;
+			return predicate(parent) || parent.HasParent(predicate);
 		}
 
-		public static void AddChildren(this GameObject parent, IEnumerable<GameObject> children) {
-			children.ForEach(child => AddChild(parent, child));
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool HasParent(this GameObject child, GameObject parent)
+			=> child.HasParent(obj => obj == parent);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static GameObject NearestParent(this GameObject child, in Func<GameObject, bool> predicate) {
+			GameObject parent = child.transform.parent.gameObject;
+			if (predicate(parent)) {
+				return parent;
+			} else {
+				return parent.NearestParent(predicate);
+			}
 		}
 
-		public static void RemoveFromParent(this GameObject child) {
-			child.transform.parent = null;
-		}
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool HasComponent<T>(this GameObject gameObject) where T : Component
+			=> gameObject.TryGetComponent(out T _);
 	}
 }
