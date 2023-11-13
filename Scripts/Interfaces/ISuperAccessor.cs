e7c882056f1db1e8a9b021e4dfcef43bb094a02c
Ryan Boyer <ryanjboyer@gmail.com>
2023-11-13 07:52:03 -0700

    Update ISuperAccessor.cs

--- Scripts/Interfaces/ISuperAccessor.cs
+++ Scripts/Interfaces/ISuperAccessor.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 namespace Foundation {
-    public interface ISuperAccessor<Super> where Super : class {
-        Super super { get; set; }
-    }
+	public interface ISuperAccessor<Super> where Super : class {
+		Super super { get; }
+	}
 }
